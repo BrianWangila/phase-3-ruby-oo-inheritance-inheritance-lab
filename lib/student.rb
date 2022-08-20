887548e5efa01531e3ae31c35af3d97927817245
Brian Wangila <bwanjalah@gmail.com>
2022-08-20 16:04:29 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative "user.rb"
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
 
 end
+student1 = Student.new
+
+pp student1.learn("JavaScript")
+pp student1.learn("JavaScript")
